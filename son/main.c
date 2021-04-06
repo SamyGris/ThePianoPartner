1aabfdbf5ce887da67c00fda45f105cecf6dafb3
reyane.en-nabty <reyane.en-nabty@epita.fr>
2021-04-06 21:28:04 +0200

    lol44

--- son/main.c
+++ son/main.c
@@ -1,20 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "inc/fmod.h"
+#include "main.h"
 
-int main()
+void jouerunenote(char *argv[])
 {   
   FMOD_SYSTEM *system;
   FMOD_SOUND *tir;
-  
   FMOD_RESULT resultat;
-  
   /* Création et initialisation d'un objet système */
   FMOD_System_Create(&system);
   FMOD_System_Init(system, 1, FMOD_INIT_NORMAL, NULL);
-  
   /* Chargement du son et vérification du chargement */
-  resultat = FMOD_System_CreateSound(system, "notepiano48/1.mp3", FMOD_CREATESAMPLE, 0, &tir);
+  resultat = FMOD_System_CreateSound(system, argv[1], FMOD_CREATESAMPLE, 0, &tir);
   if (resultat != FMOD_OK)
     {
       fprintf(stderr, "Impossible de lire pan.wav\n");
@@ -24,7 +22,6 @@ int main()
   FMOD_Sound_Release(tir);
   FMOD_System_Close(system);
   FMOD_System_Release(system);
-  return 1; 
 }
 
 
