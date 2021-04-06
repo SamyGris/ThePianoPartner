e006b34bdf0309bbfbe39d59166997fbaa47243d
Samy <samy.gris@epita.fr>
2021-04-06 19:16:09 +0200

    Définition des gammes majeures et mineures

--- notes.h
+++ notes.h
@@ -1,10 +1,7 @@
-#ifndef DEF_NOTES
-#define DEF_NOTES
+#ifndef DEF_CONSTANTES
+#define DEF_CONSTANTES
 
-enum {DOMA, REMA, MIMA, FAMA, SOLMA LAMA, SIMA,
-      DODMA, REDMA, FADMA, SOLDMA, LADMA,
-      DOMI, REMI, MIMI, FAMI, SOLMA LAMI, SIMI,
-      DODMI, REDMI, FADMI, SOLDMI, LADMI};
+// Définition des notes
 
 enum {DO1, RE1, MI1, FA1, SOL1, LA1, SI1,
       DOD1, RED1, FAD1, SOLD1, LAD1,
@@ -15,7 +12,19 @@ enum {DO1, RE1, MI1, FA1, SOL1, LA1, SI1,
       DO4, RE4, MI4, FA4, SOL4, LA4, SI4,
       DOD4, RED4, FAD4, SOLD4, LAD4};
 
+// Définition des gammes
+
+enum {DOMA, REMA, MIMA, FAMA, SOLMA LAMA, SIMA,
+      DODMA, REDMA, FADMA, SOLDMA, LADMA,
+      DOMI, REMI, MIMI, FAMI, SOLMA LAMI, SIMI,
+      DODMI, REDMI, FADMI, SOLDMI, LADMI};
+
+// Définition des notes en fonction des gammes majeures et mineures
+
 int gammes[24][7];
+
+// Gammes majeures
+
 gammes[DOMA][0] = DO1;
 gammes[DOMA][1] = RE1;
 gammes[DOMA][2] = MI1;
@@ -88,29 +97,129 @@ gammes[REDMA][4] = LA2;
 gammes[REDMA][5] = SI2;
 gammes[REDMA][6] = DOD2;
 
-gammes[FADMA][0] = RE2;
-gammes[FADMA][1] = MI2;
-gammes[FADMA][2] = FAD2;
-gammes[FADMA][3] = SOL2;
-gammes[FADMA][4] = LA2;
-gammes[FADMA][5] = SI2;
-gammes[FADMA][6] = DOD2;
+gammes[FADMA][0] = FAD1;
+gammes[FADMA][1] = SOLD1;
+gammes[FADMA][2] = LAD1;
+gammes[FADMA][3] = SI1;
+gammes[FADMA][4] = DOD2;
+gammes[FADMA][5] = RED2;
+gammes[FADMA][6] = MID2;
 
-gammes[SOLDMA][0] = RE2;
-gammes[SOLDMA][1] = MI2;
-gammes[SOLDMA][2] = FAD2;
-gammes[SOLDMA][3] = SOL2;
-gammes[SOLDMA][4] = LA2;
-gammes[SOLDMA][5] = SI2;
-gammes[SOLDMA][6] = DOD2;
+gammes[SOLDMA][0] = SOLD1;
+gammes[SOLDMA][1] = LAD1;
+gammes[SOLDMA][2] = DO2;
+gammes[SOLDMA][3] = DOD2;
+gammes[SOLDMA][4] = RED2;
+gammes[SOLDMA][5] = FA2;
+gammes[SOLDMA][6] = SOL2;
 
-gammes[LADMA][0] = RE2;
-gammes[LADMA][1] = MI2;
-gammes[LADMA][2] = FAD2;
-gammes[LADMA][3] = SOL2;
-gammes[LADMA][4] = LA2;
-gammes[LADMA][5] = SI2;
-gammes[LADMA][6] = DOD2;
+gammes[LADMA][0] = LAD1;
+gammes[LADMA][1] = DO2;
+gammes[LADMA][2] = RE2;
+gammes[LADMA][3] = RED2;
+gammes[LADMA][4] = FA2;
+gammes[LADMA][5] = SOL2;
+gammes[LADMA][6] = LA2;
 
+// Gammes mineures
+
+gammes[DOMI][0] = DO1;
+gammes[DOMI][1] = RE1;
+gammes[DOMI][2] = RED1;
+gammes[DOMI][3] = FA1;
+gammes[DOMI][4] = SOL1;
+gammes[DOMI][5] = SOLD1;
+gammes[DOMI][6] = LAD1;
+
+gammes[REMI][0] = RE1;
+gammes[REMI][1] = MI1;
+gammes[REMI][2] = FA1;
+gammes[REMI][3] = SOL1;
+gammes[REMI][4] = LA1;
+gammes[REMI][5] = LAD1;
+gammes[REMI][6] = DO1;
+
+gammes[MIMI][0] = MI1;
+gammes[MIMI][1] = FAD1;
+gammes[MIMI][2] = SOL1;
+gammes[MIMI][3] = LA1;
+gammes[MIMI][4] = SI1;
+gammes[MIMI][5] = DO2;
+gammes[MIMI][6] = RE2;
+
+gammes[FAMI][0] = FA1;
+gammes[FAMI][1] = SOL1;
+gammes[FAMI][2] = SOLD1;
+gammes[FAMI][3] = LAD2;
+gammes[FAMI][4] = DO2;
+gammes[FAMI][5] = DOD2;
+gammes[FAMI][6] = RED2;
+
+gammes[SOLMI][0] = SOL1;
+gammes[SOLMI][1] = LA1;
+gammes[SOLMI][2] = LAD1;
+gammes[SOLMI][3] = DO2;
+gammes[SOLMI][4] = RE2;
+gammes[SOLMI][5] = RED2;
+gammes[SOLMI][6] = FA2;
+
+gammes[LAMI][0] = LA1;
+gammes[LAMI][1] = SI1;
+gammes[LAMI][2] = DO2;
+gammes[LAMI][3] = RE2;
+gammes[LAMI][4] = MI2;
+gammes[LAMI][5] = FA2;
+gammes[LAMI][6] = SOL2;
+
+gammes[SIMI][0] = SI1;
+gammes[SIMI][1] = DOD2;
+gammes[SIMI][2] = RE2;
+gammes[SIMI][3] = MI2;
+gammes[SIMI][4] = FAD2;
+gammes[SIMI][5] = SOL2;
+gammes[SIMI][6] = LA2;
+
+gammes[DODMI][0] = DOD1;
+gammes[DODMI][1] = RED1;
+gammes[DODMI][2] = MI1;
+gammes[DODMI][3] = FAD1;
+gammes[DODMI][4] = SOLD1;
+gammes[DODMI][5] = LA1;
+gammes[DODMI][6] = SI1;
+
+gammes[REDMI][0] = RED1;
+gammes[REDMI][1] = FA1;
+gammes[REDMI][2] = LAD1;
+gammes[REDMI][3] = SOLD1;
+gammes[REDMI][4] = LAD1;
+gammes[REDMI][5] = SI1;
+gammes[REDMI][6] = DOD2;
+
+gammes[FADMI][0] = FAD1;
+gammes[FADMI][1] = SOLD1;
+gammes[FADMI][2] = LA1;
+gammes[FADMI][3] = SI1;
+gammes[FADMI][4] = DOD2;
+gammes[FADMI][5] = RE2;
+gammes[FADMI][6] = MI2;
+
+gammes[SOLDMI][0] = SOLD1;
+gammes[SOLDMI][1] = LAD1;
+gammes[SOLDMI][2] = SI1;
+gammes[SOLDMI][3] = DOD2;
+gammes[SOLDMI][4] = RED2;
+gammes[SOLDMI][5] = MI2;
+gammes[SOLDMI][6] = FAD2;
+
+gammes[LADMI][0] = LAD1;
+gammes[LADMI][1] = DO2;
+gammes[LADMI][2] = DOD2;
+gammes[LADMI][3] = RED2;
+gammes[LADMI][4] = FA2;
+gammes[LADMI][5] = FAD2;
+gammes[LADMI][6] = SOLD2;
+
+// A Faire : Définition des accords
+   
 
 #endif
