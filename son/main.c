d02ab043774988993d8d16a3edb113720e7e4cd5
reyane.en-nabty <reyane.en-nabty@epita.fr>
2021-04-06 22:16:37 +0200

    probleme de son a fixxé

--- son/main.c
+++ son/main.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
-//#include "son.h"
+#include "son.h"
 
 int main (int argc,char *argv[])
 {
-  //jouerduson(argv[1]);
+  jouerduson(argv[1]);
   printf("%s",argv[1]);
-  return 1;
+  return argc;
 }
 
 
--- son/son.c
+++ son/son.c
@@ -3,21 +3,22 @@
 #include "inc/fmod.h"
 #include "son.h"
 
-void jouerunenote(char *argv[])
+void jouerunenote(char *argv)
 {   
   FMOD_SYSTEM *system;
   FMOD_SOUND *tir;
   FMOD_RESULT resultat;
-  /* Création et initialisation d'un objet système */
+
   FMOD_System_Create(&system);
   FMOD_System_Init(system, 1, FMOD_INIT_NORMAL, NULL);
-  /* Chargement du son et vérification du chargement */
-  resultat = FMOD_System_CreateSound(system, argv[1], FMOD_CREATESAMPLE, 0, &tir);
+
+  resultat = FMOD_System_CreateSound(system, argv,FMOD_SOFTWARE | FMOD_2D | FMOD_CREATESTREAM, 0, &tir);
   if (resultat != FMOD_OK)
     {
       fprintf(stderr, "Impossible de lire le son\n");
       exit(EXIT_FAILURE);
     }
+  
   FMOD_System_PlaySound(system, NULL, tir, 0, NULL);
   FMOD_Sound_Release(tir);
   FMOD_System_Close(system);
