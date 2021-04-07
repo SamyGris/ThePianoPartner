cce339954a4143ee8508466a9e2946426dd8f5ea
reyane.en-nabty <reyane.en-nabty@epita.fr>
2021-04-07 18:46:27 +0200

    lol

--- son/son.c
+++ son/son.c
@@ -6,30 +6,20 @@
 
 void jouerunenote(char *argv)
 {
-   int continuer = 1;
-   SDL_Init(SDL_INIT_VIDEO);
-   SDL_Surface *ecran = NULL;
-   ecran = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
-   SDL_Event event;
+
+ 
    SDL_WM_SetCaption("SDL_Mixer", NULL);
-   SDL_Flip(ecran);
    if(Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 1024) == -1) //Initialisation de l'API Mixer
    {
       printf("%s", Mix_GetError());
    }
    Mix_Music *musique; //Création du pointeur de type Mix_Music
    musique = Mix_LoadMUS(argv); //Chargement de la musique
-   Mix_PlayMusic(musique, -1); //Jouer infiniment la musique
-   while(continuer)
-   {
-       SDL_WaitEvent(&event);
-       switch(event.type)
-       {
-       case SDL_QUIT:
-           continuer = 0;
-           break;
-       }
-   }
+   Mix_PlayMusic(musique, 1); //Jouer de la musique
+   while(1)
+     {
+       printf("boucle\n");
+     }
    Mix_FreeMusic(musique); //Libération de la musique
    Mix_CloseAudio(); //Fermeture de l'API
    SDL_Quit();
