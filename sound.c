73cd9cf2037bb5fed9c4f73763030f40841f05d1
reyane.en-nabty <reyane.en-nabty@epita.fr>
2021-05-06 19:56:55 +0200

    Ajout de commentaire

--- sound.c
+++ sound.c
@@ -1,7 +1,7 @@
 #include "sound.h"
 
 void getFrequency(int note, float* frequency, int* octave)
-{
+{//Fonction qui calcule le demi-ton à atteindre pour jouer la note souhaiter
   float douze = 1.05946;
   *octave = note/12;
   note = note % 12;
@@ -9,7 +9,7 @@ void getFrequency(int note, float* frequency, int* octave)
 }
 
 void* playNoteSound(void* arguments)
-{
+{//Fonction executer qui calcule joue le son en utilisant la bibliotheque FMOD sans prendre en compte le bpm
   struct noteData *args = arguments;
   int note = args->note;
   int inter = args->inter;
@@ -39,17 +39,16 @@ void* playNoteSound(void* arguments)
   pthread_exit(NULL);
 }
 
-/*
+
 void *playNoteSoundsec(void *arg)
-{
+{//Fonction executer qui calcule joue le son en utilisant la bibliotheque FMOD sans prendre en compte le bpm
   struct noteData *args = arg;
   int note = args->note;
   int inter =args->inter;
   float frequency;
   int octave;
-
   getFrequency(note, &frequency, &octave);
-
+  FMOD_SOUND *son;
   son =samples[octave];
   float b =2321/inter;
   float a =1/b;
@@ -69,45 +68,34 @@ void *playNoteSoundsec(void *arg)
   {
 	  errx(3,"Couldn't set the frequency");
   }
-  
-  
-    if (FMOD_DSP_SetParameterFloat(dsp_effect,0,b) != FMOD_OK)
+  updateAudio();
+  if (FMOD_DSP_SetParameterFloat(dsp_effect,0,b) != FMOD_OK)
   {
 	  errx(3,"Signal analyse : couldn't set the dsp float parametre");
   }
-
+  updateAudio();
   if (FMOD_Channel_AddDSP(channel,0,dsp_effect) != FMOD_OK)
   {
 	  errx(3,"Couldn't add the DSP to the channel");
   }
+  updateAudio();
   msleep(inter);
   pthread_exit(NULL);
-    if (FMOD_Sound_Release(son) != FMOD_OK)
-  {
-    errx(3, "Couldn't release the sound");
-  }
-    if (FMOD_System_Update(systemSound)!=FMOD_OK)
-  {
-    errx(3,"error during system update");
-  }
   return NULL;
-  
-}*/
+}
 
 void initAudio()
-{
+{//Fonction qui va initialiser les systemes et les variables necessaires pour la lecture de son
   // Initialisation et création de notre système    
   if (FMOD_System_Create(&systemSound) != FMOD_OK)
   {
     errx(3,"Couldn't create a system");
   }
-  
   // Initialisation du système en mode lecture normale de son et à 32 channels
   if (FMOD_System_Init(systemSound,32,FMOD_INIT_NORMAL,NULL) != FMOD_OK)
   {
     errx(3,"Couldn't init a system");
   }
-
   // Chargement de la banque sonore
   FMOD_SOUND *sound;
   if (FMOD_System_CreateSound(systemSound,"notes/DO1.wav",FMOD_CREATESAMPLE,0, &sound) != FMOD_OK)
@@ -115,33 +103,29 @@ void initAudio()
     errx(3,"Couldn't create DO1.wav sound");
   }
   samples[0] = sound;
-
-
   if (FMOD_System_CreateSound(systemSound,"notes/DO2.wav",FMOD_CREATESAMPLE,0,&sound) != FMOD_OK)
   {
     errx(3,"Couldn't create DO2.wav sound");
   }
   samples[1] = sound;
-
   if (FMOD_System_CreateSound(systemSound,"notes/DO3.wav",FMOD_CREATESAMPLE,0,&sound) != FMOD_OK)
   {
     errx(3,"Couldn't create DO3.wav sound");
   }
   samples[2] = sound; 
-
   if (FMOD_System_CreateSound(systemSound,"notes/DO4.wav",FMOD_CREATESAMPLE,0,&sound) != FMOD_OK)
   {
     errx(3,"Couldn't create DO4.wav sound");
   }
   samples[3] = sound; 
-
   if (FMOD_System_CreateDSPByType(systemSound,FMOD_DSP_TYPE_PITCHSHIFT,&dsp_effect) != FMOD_OK)
   {
 	  errx(3,"Signal analyse : couldn't create a dsp ");
   }
 }
+
 void updateAudio()
-{
+{//Fonction qui update le system sonore
   if (FMOD_System_Update(systemSound)!=FMOD_OK)
   {
     errx(3,"error during system update");
